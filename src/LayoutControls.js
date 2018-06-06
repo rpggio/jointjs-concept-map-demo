f175a000747c0252992082b0e9fff49abdb83c08
Ryan Smith <ryan@finetunelearning.com>
2018-06-05 17:03:09 -0700

    Directed layout working

--- src/LayoutControls.js
+++ src/LayoutControls.js
@@ -1,4 +1,105 @@
-import joint, { Shape, Link } from 'jointjs';
+import joint from 'jointjs';
+
+var Shape = joint.dia.Element.define('demo.Shape', {
+  size: {
+    width: 100,
+    height: 50
+  },
+  attrs: {
+    rect: {
+      refWidth: '100%',
+      refHeight: '100%',
+      fill: 'ivory',
+      stroke: 'gray',
+      strokeWidth: 2,
+      rx: 10,
+      ry: 10
+    },
+    text: {
+      refX: '50%',
+      refY: '50%',
+      yAlignment: 'middle',
+      xAlignment: 'middle',
+      fontSize: 30
+    }
+  }
+}, {
+    markup: '<rect/><text/>',
+
+    setText: function (text) {
+      return this.attr('text/text', text || '');
+    }
+  });
+
+var Link = joint.dia.Link.define('demo.Link', {
+  attrs: {
+    '.connection': {
+      stroke: 'gray',
+      strokeWidth: 2,
+      pointerEvents: 'none',
+      targetMarker: {
+        type: 'path',
+        fill: 'gray',
+        stroke: 'none',
+        d: 'M 10 -10 0 0 10 10 z'
+      }
+    }
+  },
+  connector: {
+    name: 'rounded'
+  },
+  z: -1,
+  weight: 1,
+  minLen: 1,
+  labelPosition: 'c',
+  labelOffset: 10,
+  labelSize: {
+    width: 50,
+    height: 30
+  },
+  labels: [{
+    markup: '<rect/><text/>',
+    attrs: {
+      text: {
+        fill: 'gray',
+        textAnchor: 'middle',
+        refY: 5,
+        refY2: '-50%',
+        fontSize: 20,
+        cursor: 'pointer'
+      },
+      rect: {
+        fill: 'lightgray',
+        stroke: 'gray',
+        strokeWidth: 2,
+        refWidth: '100%',
+        refHeight: '100%',
+        refX: '-50%',
+        refY: '-50%',
+        rx: 5,
+        ry: 5
+      }
+    },
+    size: {
+      width: 50, height: 30
+    }
+  }]
+
+}, {
+    markup: '<path class="connection"/><g class="labels"/>',
+
+    connect: function (sourceId, targetId) {
+      return this.set({
+        source: { id: sourceId },
+        target: { id: targetId }
+      });
+    },
+
+    setLabelText: function (text) {
+      return this.prop('labels/0/attrs/text/text', text || '');
+    }
+  });
+
 
 var LayoutControls = joint.mvc.View.extend({
 
