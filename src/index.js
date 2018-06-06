c8c6a0062321babcf10248ca7fcfb01e33fe0b01
Ryan Smith <ryan@finetunelearning.com>
2018-06-05 17:15:36 -0700

    Loading diagram YAML

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import joint from 'jointjs';
 import 'jointjs/dist/joint.css';
+import yaml from 'js-yaml';
 
 import LayoutControls from './LayoutControls';
+import diagramYml from '../data/diagram.yml';
+
+const diagram = yaml.safeLoad(diagramYml);
 
 const appElement = document.getElementById("app");
 
